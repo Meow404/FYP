3df92f95ae17a2b6dc9973432d8c966c2ea8333c
Meow404 <stephenthomasfelix@yahoo.com>
2021-03-09 22:34:29 +0800

    formatting

--- CUDAC/cnn.cu
+++ CUDAC/cnn.cu
@@ -9,10 +9,9 @@
 #include "imageConvolutionParallelConstantMemory.h"
 #include "imageConvolutionTextureMemory.h"
 
-
- const char *imageFilename = "lena_bw.pgm";
+const char *imageFilename = "res//images//lena_bw.pgm";
 //const char *imageFilename = "galaxy.ascii.pgm";
-#define KERNELDIMENSION 3 
+#define KERNELDIMENSION 3
 
 int main(int argc, char **argv)
 {
@@ -22,36 +21,35 @@ int main(int argc, char **argv)
   printf("2 - Naive parallel implementation \n");
   printf("3 - Shared memory implementation \n");
   printf("4 - Constant memory implementation \n");
-  printf("5 - Texture memory implementation \n ");  
+  printf("5 - Texture memory implementation \n ");
   int option;
   scanf("%d", &option);
 
-  switch(option) {
-    case 1  :
-      imageConvolutionSerial(imageFilename,argv);
-      break; 
-   
-    case 2  :
-       imageConvolutionParallel(imageFilename,argv);
-       break; 
+  switch (option)
+  {
+  case 1:
+    imageConvolutionSerial(imageFilename, argv);
+    break;
 
-    case 3  :
-      imageConvolutionParallelSharedMemory(imageFilename,argv);
-      break;
+  case 2:
+    imageConvolutionParallel(imageFilename, argv);
+    break;
 
-    case 4:
-	imageConvolutionParallelConstantMemory(imageFilename,argv);
-	break; 
-    
-    case 5:
-	imageConvolutionParallelTextureMomory(imageFilename,argv);
-	break;
+  case 3:
+    imageConvolutionParallelSharedMemory(imageFilename, argv);
+    break;
 
-    default : 
-      printf("Incorrect input \n");
- }
+  case 4:
+    imageConvolutionParallelConstantMemory(imageFilename, argv);
+    break;
+
+  case 5:
+    imageConvolutionParallelTextureMomory(imageFilename, argv);
+    break;
+
+  default:
+    printf("Incorrect input \n");
+  }
 
   return 0;
 }
-
-
