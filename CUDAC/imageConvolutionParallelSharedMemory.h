a7c31e548c7aba431ce91755f0d5468d9d052264
Meow404 <stephenthomasfelix@yahoo.com>
2021-03-17 15:41:38 +0800

    bugs

--- CUDAC/imageConvolutionParallelSharedMemory.h
+++ CUDAC/imageConvolutionParallelSharedMemory.h
@@ -97,7 +97,7 @@ __global__ void applyKernelPerPixelParallelSharedMemory(int *d_kernelDimensionX,
   //   printf("")
   // }
 
-  if (threadIdx.x >= offsetX && threadIdx.x < blockDim.x - offsetX && threadIdx.y >= offsetY && threadIdx.y < blockDim.y - offsetY) || (blockIdx.x == 0 && threadIdx.y < offsetY) || (blockIdx.y == 0 && threadIdx.x < offsetX))
+  if ((threadIdx.x >= offsetX && threadIdx.x < blockDim.x - offsetX && threadIdx.y >= offsetY && threadIdx.y < blockDim.y - offsetY) || (blockIdx.x == 0 && threadIdx.y < offsetY) || (blockIdx.y == 0 && threadIdx.x < offsetX))
   {
 
     if ((y < (*d_imageHeight)) && (x < (*d_imageWidth)))
