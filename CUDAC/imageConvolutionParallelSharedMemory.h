ec661a51903f7c0f8b37261bc94cde172c3cdb22
Meow404 <stephenthomasfelix@yahoo.com>
2021-03-17 14:26:52 +0800

    fixed

--- CUDAC/imageConvolutionParallelSharedMemory.h
+++ CUDAC/imageConvolutionParallelSharedMemory.h
@@ -33,8 +33,8 @@ float *applyKernelToImageParallelSharedMemory(float *image, int imageWidth, int 
   cudaMemcpy(d_kernel, kernel.matrix, kernel.dimension * kernel.dimension * sizeFloat, cudaMemcpyHostToDevice);
   cudaMemcpy(d_image, image, sizeImageArray, cudaMemcpyHostToDevice);
 
-  int overlapX = (kernel.dimension - 1) / 2;
-  int overlapY = (kernel.dimension - 1) / 2;
+  int overlapX = (kernel.dimension + 1) / 2;
+  int overlapY = (kernel.dimension + 1) / 2;
 
   int numHorBlocks = (imageWidth) / (blockWidth - overlapX);
   int numVerBlocks = (imageHeight) / (blockWidth - overlapY );
