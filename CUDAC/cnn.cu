588bbd7b6e4a105ee2b1218af08e16eccebc8039
Meow404 <stephenthomasfelix@yahoo.com>
2021-03-18 20:10:26 +0800

    tables

--- CUDAC/cnn.cu
+++ CUDAC/cnn.cu
@@ -17,13 +17,14 @@ const char *imageFilename = "res//images//lena_bw.pgm";
 #define ITERATIONS 20
 #define BLOCK_WIDTH 16
 
-void imageConvolutionParallel(const char *imageFilename, char **argv, int option)
+float *imageConvolutionParallel(const char *imageFilename, char **argv, int option, kernel **kernels, int numOfKernels, bool print_save = true)
 {
   // load image from disk
   float *hData = NULL;
   char buf[512];
   unsigned int width, height;
   char *imagePath = sdkFindFilePath(imageFilename, argv[0]);
+  float *results;
 
   if (imagePath == NULL)
   {
@@ -32,7 +33,10 @@ void imageConvolutionParallel(const char *imageFilename, char **argv, int option
   }
 
   sdkLoadPGM(imagePath, &hData, &width, &height);
-  printf("Loaded '%s', %d x %d pixels\n", imageFilename, width, height);
+  if (print_save)
+    printf("Loaded '%s', %d x %d pixels\n", imageFilename, width, height);
+
+  results = (float *)malloc(numOfKernels * sizeof(float));
 
   //Get Kernels
   // FILE *fp = fopen("kernels.txt", "r");
@@ -41,6 +45,120 @@ void imageConvolutionParallel(const char *imageFilename, char **argv, int option
   //   perror("Error in opening file");
   //   exit(EXIT_FAILURE);
   // }
+
+  for (int i = 0; i < numOfKernels; i++)
+  {
+
+    char outputFilename[1024];
+    char *file_name;
+
+    float *result;
+    float totalTime = 0.0;
+    if (print_save)
+      printf("\n\n\nKernel Dimension : %dx%d\n", kernels[i]->dimension, kernels[i]->dimension);
+
+    for (int j = 0; j < ITERATIONS; j++)
+    {
+      cudaDeviceReset();
+      cudaEvent_t start, stop;
+      cudaEventCreate(&start);
+      cudaEventCreate(&stop);
+      cudaEventRecord(start);
+
+      switch (option)
+      {
+      case 1:
+        result = applyKernelToImageSerial(hData, width, height, *kernels[i], imagePath);
+        if (j == 0)
+          sprintf(file_name, "_%dx%d_serial_out.pgm", kernels[i]->dimension, kernels[i]->dimension);
+        break;
+
+      case 2:
+        result = applyKernelToImageParallelNaive(hData, width, height, kernels[i], imagePath, BLOCK_WIDTH);
+        if (j == 0)
+          sprintf(file_name, "_%dx%d_parallel_out.pgm", kernels[i]->dimension, kernels[i]->dimension);
+        break;
+
+      case 3:
+        result = applyKernelToImageParallelSharedMemory(hData, width, height, *kernels[i], imagePath, BLOCK_WIDTH);
+        if (j == 0)
+          sprintf(file_name, "_%dx%d_parallel_shared_out.pgm", kernels[i]->dimension, kernels[i]->dimension);
+        break;
+
+      case 4:
+        result = applyKernelToImageParallelConstantMemory(hData, width, height, *kernels[i], imagePath, BLOCK_WIDTH);
+        if (j == 0)
+          sprintf(file_name, "_%dx%d_parallel_constant_out.pgm", kernels[i]->dimension, kernels[i]->dimension);
+        break;
+
+      case 5:
+        result = applyKernelToImageParallelSharedConstantMemory(hData, width, height, *kernels[i], imagePath, BLOCK_WIDTH);
+        if (j == 0)
+          sprintf(file_name, "_%dx%d_parallel_shared_constant_out.pgm", kernels[i]->dimension, kernels[i]->dimension);
+        break;
+
+      case 6:
+        result = applyKernelToImageParallelTextureMomory(hData, width, height, *kernels[i], imagePath, BLOCK_WIDTH);
+        if (j == 0)
+          sprintf(file_name, "_%dx%d_parallel_texture_out.pgm", kernels[i]->dimension, kernels[i]->dimension);
+        break;
+
+      case 7:
+        result = applyKernelToImageParallel2DTextureMomory(hData, width, height, *kernels[i], imagePath, BLOCK_WIDTH);
+        if (j == 0)
+          sprintf(file_name, "_%dx%d_paralled_2D_texture_out.pgm", kernels[i]->dimension, kernels[i]->dimension);
+        break;
+
+      default:
+        printf("Incorrect input \n");
+      }
+      cudaEventRecord(stop);
+      cudaEventSynchronize(stop);
+      float milliseconds = 0;
+      cudaEventElapsedTime(&milliseconds, start, stop);
+      if (print_save)
+        printf("Time Naive Parallel Implementation: %f \n", milliseconds);
+      totalTime += milliseconds;
+    }
+    if (print_save)
+      printf("Time Serial Average Implementation: %f ms\n", totalTime / ITERATIONS);
+
+    results[i] = totalTime / ITERATIONS;
+    if (print_save)
+    {
+      for (int j = 0; j < height; j++)
+      {
+        printf("[%3d] : ", j);
+        for (int i = 0; i < width; i++)
+        {
+          printf(" |%5.2f|", result[j * width + i]);
+        }
+        printf("\n");
+      }
+      strcpy(outputFilename, imagePath);
+      strcpy(outputFilename + strlen(imagePath) - 4, file_name);
+      printf("Saving to %s", outputFilename);
+      sdkSavePGM(outputFilename, result, width, height);
+    }
+  }
+  return results;
+}
+
+int main(int argc, char **argv)
+{
+  printf("Image convolution project \n");
+  printf("Please select an option \n");
+  printf("1 - Serial Implementation \n");
+  printf("2 - Naive parallel implementation \n");
+  printf("3 - Shared memory implementation \n");
+  printf("4 - Constant memory implementation \n");
+  printf("5 - Shared Constant memory implementation \n");
+  printf("6 - Texture memory implementation \n ");
+  printf("7 - 2D Texture memory implementation \n ");
+  printf("8 - All \n ");
+  int option;
+  char buf[512];
+  scanf("%d", &option);
 
   FILE *fp = fopen("kernels.txt", "r");
   if (fp == NULL)
@@ -56,108 +174,33 @@ void imageConvolutionParallel(const char *imageFilename, char **argv, int option
   printf("%d kernel to be loaded\n", numOfKernels);
 
   kernel **kernels = loadAllKernels(fp, numOfKernels);
+  printKernels(kernels, numOfKernels);
 
   // kernelHandler kh = kernelHandler("../kernels.txt");
 
   printf("Kernels loaded\n");
 
-  for (int i = 0; i < numOfKernels; i++)
+  if (option < 8)
+    imageConvolutionParallel(imageFilename, argv, option, kernels, numOfKernels);
+  else if (option == 8)
   {
+    float **results = (float **)malloc(sizeof(float *) * 7);
+    for (int i = 1; i < 8; i++)
+      results[i] = imageConvolutionParallel(imageFilename, argv, option, kernels, numOfKernels, false);
 
-    char outputFilename[1024];
-    char* file_name;
-    
-    float *result;
-    float totalTime = 0.0;
-    printf("\n\n\nKernel Dimension : %dx%d\n", kernels[i]->dimension, kernels[i]->dimension);
-
-    for (int j = 0; j < ITERATIONS; j++)
+    printf("| MxM | Serial |Parallel| Shared |Constant|   SC   |  Text  | 2DText |");
+    for (int i = 0; i < numOfKernels; i++)
     {
-      cudaDeviceReset();
-      cudaEvent_t start, stop;
-      cudaEventCreate(&start);
-      cudaEventCreate(&stop);
-      cudaEventRecord(start);
-
-      switch (option)
+      printf("|%2dx%2d|", kernels[i]->dimension, kernels[i]->dimension);
+      for (int j = 1; j < 8; j++)
       {
-      case 1:
-        result = applyKernelToImageSerial(hData, width, height, *kernels[i], imagePath);
-        if (j == 0) sprintf(file_name, "_%dx%d_serial_out.pgm", kernels[i]->dimension, kernels[i]->dimension);
-        break;
-
-      case 2:
-        result = applyKernelToImageParallelNaive(hData, width, height, kernels[i], imagePath, BLOCK_WIDTH);
-        if (j == 0) sprintf(file_name, "_%dx%d_parallel_out.pgm", kernels[i]->dimension, kernels[i]->dimension);
-        break;
-
-      case 3:
-        result = applyKernelToImageParallelSharedMemory(hData, width, height, *kernels[i], imagePath, BLOCK_WIDTH);
-        if (j == 0) sprintf(file_name, "_%dx%d_parallel_shared_out.pgm", kernels[i]->dimension, kernels[i]->dimension);
-        break;
-
-      case 4:
-        result = applyKernelToImageParallelConstantMemory(hData, width, height, *kernels[i], imagePath, BLOCK_WIDTH);
-        if (j == 0) sprintf(file_name, "_%dx%d_parallel_constant_out.pgm", kernels[i]->dimension, kernels[i]->dimension);
-        break;
-
-      case 5:
-        result = applyKernelToImageParallelSharedConstantMemory(hData, width, height, *kernels[i], imagePath, BLOCK_WIDTH);
-        if (j == 0) sprintf(file_name, "_%dx%d_parallel_shared_constant_out.pgm", kernels[i]->dimension, kernels[i]->dimension);
-        break;
-
-      case 6:
-        result = applyKernelToImageParallelTextureMomory(hData, width, height, *kernels[i], imagePath, BLOCK_WIDTH);
-        if (j == 0) sprintf(file_name, "_%dx%d_parallel_texture_out.pgm", kernels[i]->dimension, kernels[i]->dimension);
-        break;
-
-      case 7:
-        result = applyKernelToImageParallel2DTextureMomory(hData, width, height, *kernels[i], imagePath, BLOCK_WIDTH);
-        if (j == 0) sprintf(file_name, "_%dx%d_paralled_2D_texture_out.pgm", kernels[i]->dimension, kernels[i]->dimension);
-        break;
-
-      default:
-        printf("Incorrect input \n");
-      }
-      cudaEventRecord(stop);
-      cudaEventSynchronize(stop);
-      float milliseconds = 0;
-      cudaEventElapsedTime(&milliseconds, start, stop);
-      printf("Time Naive Parallel Implementation: %f \n", milliseconds);
-      totalTime += milliseconds;
-    }
-    printf("Time Serial Average Implementation: %f ms\n", totalTime / ITERATIONS);
-
-    for (int j = 0; j < height; j++)
-    {
-      printf("[%3d] : ", j);
-      for (int i = 0; i < width; i++)
-      {
-        printf(" |%5.2f|", result[j * width + i]);
+        printf("%8.3f|");
       }
       printf("\n");
     }
-    strcpy(outputFilename, imagePath);
-    strcpy(outputFilename + strlen(imagePath) - 4, file_name);
-    printf("Saving to %s", outputFilename);
-    sdkSavePGM(outputFilename, result, width, height);
   }
-}
-
-int main(int argc, char **argv)
-{
-  printf("Image convolution project \n");
-  printf("Please select an option \n");
-  printf("1 - Serial Implementation \n");
-  printf("2 - Naive parallel implementation \n");
-  printf("3 - Shared memory implementation \n");
-  printf("4 - Constant memory implementation \n");
-  printf("5 - Shared Constant memory implementation \n");
-  printf("6 - Texture memory implementation \n ");
-  int option;
-  scanf("%d", &option);
-
-  imageConvolutionParallel(imageFilename, argv, option);
+  else
+    printf("\n\nInvalid Input !!!");
   // switch (option)
   // {
   // case 1:
--- CUDAC/helpers.h
+++ CUDAC/helpers.h
@@ -1,14 +1,14 @@
-#ifndef _helper_h   
+#ifndef _helper_h
 #define _helper_h
 
 #include <iostream>
 #include <stdio.h>
 
-void flipKernel(float* kernel, int kernelDimension);
-void loadKernels(float * kernel, char buf[512]);
-void loadAllKernels(float ** kernels,  FILE* fp);
-int getNumKernels(FILE* fp);
-void loadRow(float * matrix, int row, int kernel_dim, char buf[512]);
+void flipKernel(float *kernel, int kernelDimension);
+void loadKernels(float *kernel, char buf[512]);
+void loadAllKernels(float **kernels, FILE *fp);
+int getNumKernels(FILE *fp);
+void loadRow(float *matrix, int row, int kernel_dim, char buf[512]);
 
 struct kernel
 {
@@ -16,7 +16,8 @@ struct kernel
     float *matrix;
 };
 
-kernel** loadAllKernels(FILE* fp, int numOfKernels){   
+kernel **loadAllKernels(FILE *fp, int numOfKernels)
+{
     char buf[512];
 
     kernel **kernels = (kernel **)malloc(sizeof(kernel *) * numOfKernels);
@@ -39,111 +40,116 @@ kernel** loadAllKernels(FILE* fp, int numOfKernels){
 
             loadRow(kernels[i]->matrix, j, kernel_dim, buf);
         }
-        
+
         // kernels[i] = &kl;
     }
 
-    
     printf("\nNum of kernels : %d", numOfKernels);
     for (int k = 0; k < numOfKernels; k++)
     {
         float sum = 0.0;
         for (int i = 0; i < kernels[k]->dimension; i++)
             for (int j = 0; j < kernels[k]->dimension; j++)
-                sum += kernels[k]->matrix[i*kernels[k]->dimension +j];
+                sum += kernels[k]->matrix[i * kernels[k]->dimension + j];
 
+        for (int i = 0; i < kernels[k]->dimension; i++)
+            for (int j = 0; j < kernels[k]->dimension; j++)
+                kernels[k]->matrix[i * kernels[k]->dimension + j] /= sum;
+    }
+    return kernels;
+
+    // while (fgets(buf, sizeof(buf), fp) != NULL)
+    // {
+    //   loadKernels(kernels[index],buf);
+    //   index++;
+    // }
+}
+
+void loadRow(float *matrix, int row, int kernel_dim, char buf[512])
+{
+    int count = 0;
+    buf[strlen(buf) - 1] = '\0';
+    const char delimeter[2] = ",";
+    char *token = strtok(buf, delimeter);
+    while (token != NULL)
+    {
+        sscanf(token, "%f,", &matrix[row * kernel_dim + count]);
+        token = strtok(NULL, delimeter);
+        count = count + 1;
+    }
+}
+
+//https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/
+void flipKernel(float *kernel, int kernelDimension)
+{
+    int temp;
+    int start = 0;
+    int end = kernelDimension * kernelDimension - 1;
+    while (start < end)
+    {
+        temp = kernel[start];
+        kernel[start] = kernel[end];
+        kernel[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void printImage(float *image, int width, int height, char *fileName)
+{
+    FILE *f = fopen(fileName, "w");
+    if (f == NULL)
+    {
+        printf("Error opening file!\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            fprintf(f, "%f ", image[i * width + j]);
+            if ((i * width + j) % 12 == 0)
+            {
+                fprintf(f, "\n");
+            }
+        }
+        fprintf(f, "\n");
+    }
+}
+
+void printKernels(kernel **kernels, int numOfKernels)
+{
+    for (int k = 0; k < numOfKernels; k++)
+    {
         printf("\n%dx%d", kernels[k]->dimension, kernels[k]->dimension);
         for (int i = 0; i < kernels[k]->dimension; i++)
         {
             printf("\n");
             for (int j = 0; j < kernels[k]->dimension; j++)
             {
-                kernels[k]->matrix[i*kernels[k]->dimension +j] /= sum;
-                printf("%3.2f ", kernels[k]->matrix[i*kernels[k]->dimension + j]);
+                kernels[k]->matrix[i * kernels[k]->dimension + j] /= sum;
+                printf("%3.2f ", kernels[k]->matrix[i * kernels[k]->dimension + j]);
             }
         }
         std::cout << "\n====================================";
     }
-    return kernels;
-   
-    // while (fgets(buf, sizeof(buf), fp) != NULL)
-    // {
-    //   loadKernels(kernels[index],buf);
-    //   index++;
-    // }
-      
 }
 
-void loadRow(float * matrix, int row, int kernel_dim, char buf[512]){ 
-    int count = 0;
-    buf[strlen(buf) - 1] = '\0';
-    const char delimeter[2] = ",";
-    char* token = strtok(buf, delimeter);
-    while( token != NULL){
-      sscanf(token, "%f,", &matrix[row*kernel_dim + count] );
-      token = strtok(NULL,delimeter);
-      count = count + 1;
-        
-    } 
-}
-
-//https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/
-void flipKernel(float* kernel, int kernelDimension){
-  int temp;
-  int start = 0;
-  int end = kernelDimension * kernelDimension - 1;
-  while (start < end)
-  {
-      temp = kernel[start];   
-      kernel[start] = kernel[end];
-      kernel[end] = temp;
-      start++;
-      end--;
-  }   
-      
-}
-
-void printImage(float *image,int width,int height,char * fileName){
-   FILE *f = fopen(fileName, "w");
-   if (f == NULL)
-   {
-    printf("Error opening file!\n");
-    exit(1);
-   }
-
-   for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            fprintf(f,"%f ", image[i*width+j]);
-            if((i*width+j) % 12 == 0){
-               fprintf(f,"\n");
-           }
-        }
-        fprintf(f,"\n");
-    }
-}
-
-void printKernel(float* kernel, int kernelDimension){
-  for (int i = 0; i < kernelDimension; i++) {
-       for (int j = 0; j < kernelDimension; j++) {
-           printf("%f ", kernel[i*kernelDimension+j]);
-       }
-       printf("\n");
-   }
-}
-
-int getNumKernels(FILE* fp){
+int getNumKernels(FILE *fp)
+{
     return 1;
-//   int ch, lines=0;
-//   while(!feof(fp))
-//   {
-//        printf("2");
-//       ch = fgetc(fp);
-//       if(ch == '\n')
-//       {
-//         lines++;
-//       }
-//   }
-//   rewind(fp);
-//   return lines;
+    //   int ch, lines=0;
+    //   while(!feof(fp))
+    //   {
+    //        printf("2");
+    //       ch = fgetc(fp);
+    //       if(ch == '\n')
+    //       {
+    //         lines++;
+    //       }
+    //   }
+    //   rewind(fp);
+    //   return lines;
 }
-#endif 
+#endif
