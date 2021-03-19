69a3d764c414a45ab3bfd03d372e86fb85da2b6f
Meow404 <stephenthomasfelix@yahoo.com>
2021-03-19 09:03:54 +0800

    freeing more

--- CUDAC/helpers.h
+++ CUDAC/helpers.h
@@ -65,6 +65,22 @@ kernel **loadAllKernels(FILE *fp, int numOfKernels)
     // }
 }
 
+void freeKernels(kernel **kernels, int numOfKernels)
+{
+    for (int k = 0; k < numOfKernels; k++)
+    {
+        for (int i = 0; i < kernels[k]->dimension; i++)
+        {
+            for (int j = 0; j < kernels[k]->dimension; j++)
+            {
+                free(kernels[k]->matrix);
+            }
+        }
+        free(kernels[k]);
+    }
+    free(kernels);
+}
+
 void loadRow(float *matrix, int row, int kernel_dim, char buf[512])
 {
     int count = 0;
