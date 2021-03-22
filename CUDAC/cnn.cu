1456adebd5df8b014ff255398b9507126cce312f
Meow404 <stephenthomasfelix@yahoo.com>
2021-03-22 12:42:28 +0800

    removed prints

--- CUDAC/cnn.cu
+++ CUDAC/cnn.cu
@@ -189,10 +189,10 @@ float *imageConvolutionParallel(const char *imageFilename, char **argv, int opti
         }
         printf("\n");
       }
-      strcpy(outputFilename, imagePath);
-      strcpy(outputFilename + strlen(imagePath) - 4, file_name);
-      printf("Saving to %s", outputFilename);
-      sdkSavePGM(outputFilename, result, width, height);
+      // strcpy(outputFilename, imagePath);
+      // strcpy(outputFilename + strlen(imagePath) - 4, file_name);
+      // printf("Saving to %s", outputFilename);
+      // sdkSavePGM(outputFilename, result, width, height);
       free(result);
     }
   }
